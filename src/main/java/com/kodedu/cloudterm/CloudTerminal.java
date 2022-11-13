bafde58ef799aef8dc1e09d2fd7fe6208f3f0996
Javie <jiawei_wang@yunzhijia.com>
2022-11-13 20:45:56 +0800

    rename AppStarter

--- src/main/java/com/kodedu/cloudterm/CloudTerminal.java
+++ src/main/java/com/kodedu/cloudterm/CloudTerminal.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppStarter {
+public class CloudTerminal {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppStarter.class, args);
+		SpringApplication.run(CloudTerminal.class, args);
 	}
 }
